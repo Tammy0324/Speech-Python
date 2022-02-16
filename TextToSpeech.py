606da0f3c9f07754c90ea9eb54f1db888de9cf0f
Tammy0324 <u102314@gmail.com>
2022-02-16 18:13:45 +0800

    Update TextToSpeech.py
    隨機選取文章

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,23 +1,51 @@
+#from ast import Num
+#from re import X
 import azure.cognitiveservices.speech as speechsdk
 import requests
 from bs4 import BeautifulSoup
+import random
 
 speech_config = speechsdk.SpeechConfig(subscription="b751009eb5f545f8a4d0ce3cab8d7c71", region="eastasia")
 
 speech_config.speech_synthesis_language = "en-US"
 
-r=requests.get("https://www.eslfast.com/kidsenglish/ke/ke001.htm")
+#from urllib.parse import urlparse
+#import requests
+#from bs4 import BeautifulSoup 
+
+url = "https://www.eslfast.com/kidsenglish/ke/ke{}{}{}.htm"
+
+a = random.choice('0')
+b = random.choice('1234567890')
+c = random.choice('1234567890')
+
+#for t in  :
+  #print(url.format(t))
+r=requests.get(url.format(a,b,c))
 soup=BeautifulSoup(r.text, "html.parser")
 sel=soup.select('div p.read_text font')
-
+#type(sel)
 string = ""
-
 for s in sel:
   string = s.text
-  
-print(string)
+print(s.text)
 
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-
 synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 synthesizer.speak_text_async(string)
+
+# r=requests.get("https://www.eslfast.com/kidsenglish/ke/ke001.htm")
+# soup=BeautifulSoup(r.text, "html.parser")
+# sel=soup.select('div p.read_text font')
+
+# string = ""
+
+# for s in sel:
+#   string = s.text
+  
+# print(string)
+
+# audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+# synthesizer.speak_text_async(string)
