08783620590cafcc49c30e21b0b4331c1661d878
Tammy0324 <u102314@gmail.com>
2022-02-16 18:13:29 +0800

    Update SpeechToText.py
    手動切割需要的字串

--- SpeechToText.py
+++ SpeechToText.py
@@ -9,16 +9,25 @@ speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audi
 done = False
     
 def stop_cb(evt):
-    print('CLOSING on {}'.format(evt))
+    #print('CLOSING on {}'.format(evt))
     speech_recognizer.stop_continuous_recognition()
     global done
     done = True
 
-speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
-speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
-speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
-speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+def cutString(evt):
+    evt = str(evt)
+    arr = evt.split(', ')
+    #print(arr[2])
+    text = arr[2]
+    #print(text)
+    arr2 = text.split('"')
+    print(arr2[1])
+
+#speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+speech_recognizer.recognized.connect(lambda evt: cutString(evt))
+#speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+#speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+#speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
 
 speech_recognizer.session_stopped.connect(stop_cb)
 speech_recognizer.canceled.connect(stop_cb)
