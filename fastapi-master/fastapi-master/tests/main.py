855aabdd24ffbc8c74ef4a30ab610f6adb993bb9
Tammy0324 <u102314@gmail.com>
2022-02-23 11:39:41 +0800

    Update main.py

--- fastapi-master/fastapi-master/tests/main.py
+++ fastapi-master/fastapi-master/tests/main.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
 
 app = FastAPI()
 
 @app.post('/items/')
-async def create_item(item: Item):
-    return item
+async def read_items(q: Optional[str] = Query(None, max_length = 50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
