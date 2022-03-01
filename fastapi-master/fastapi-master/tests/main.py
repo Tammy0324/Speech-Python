121b4c20ab68f5b483608caefdaab704b684e258
Tammy0324 <u102314@gmail.com>
2022-03-01 15:49:33 +0800

    Update main.py

--- fastapi-master/fastapi-master/tests/main.py
+++ fastapi-master/fastapi-master/tests/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+import requests
+from bs4 import BeautifulSoup
+import random
 
 app = FastAPI()
 
-@app.post('/items/')
-async def create_item(item: Item):
-    return item
+url = "https://www.eslfast.com/kidsenglish/ke/ke{}{}{}.htm"
+
+a = random.choice('0')
+b = random.choice('1234567890')
+c = random.choice('1234567890')
+
+r=requests.get(url.format(a,b,c))
+soup=BeautifulSoup(r.text, "html.parser")
+sel=soup.select('div p.read_text font')
+string = ""
+for s in sel:
+  string = s.text
+arr = string.split('\n')
+string = arr[2]
+print(string)
+@app.get('/')
+def create_item():
+    return string
