7d340955c1ccd6dbd0bdf2ba15ea941ee1aaf6b0
chouai925 <sandy61115@gmail.com>
2022-05-23 10:59:02 +0800

    upload and save file

--- fastapi-master/fastapi-master/tests/main.py
+++ fastapi-master/fastapi-master/tests/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+from typing import List
 
 app = FastAPI()
 
+@app.post('/')
+async def root(file: UploadFile = File(...)):
+    with open(f'{file.filename}',"wb") as buffer:
+        shutil.copyfileobj(file.file,buffer)
+    return {"file_name":file.filename}
+
+# @app.post('/play')
+# async def upload_play(file: List[UploadFile] = File(...)):
+#     for play in files:
+#         with open(f'{play.filename}',"wb") as buffer:
+#             shutil.copyfileobj(file.file,buffer)
+#
+#     return {"file_name":file.filename}
+
 @app.get('/')
 def index():
-    return 'hi!!! Sandy'
+    return 'hi!!!'
